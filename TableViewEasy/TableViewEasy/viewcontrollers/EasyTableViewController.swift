d2b53d1258ffbb1088df9c15bd1371f806a94828
PhyoThiha-MMDS <phyo.thiha@mm-digital-solutions.com>
2019-07-29 10:25:25 +0630

    Intial Commit

---
+++ TableViewEasy/TableViewEasy/viewcontrollers/EasyTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EasyTableViewController.swift
+//  TableViewEasy
+//
+//  Created by Phyo Thiha on 29/07/2019.
+//  Copyright © 2019 padc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EasyTableViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    
+}
